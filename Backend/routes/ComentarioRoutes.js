f508abca4be83ae885719addfe99514129302b27
Rodrigo Yap <rodyap182@gmail.com>
2022-03-25 16:23:02 -0600

    Se quitaron discrepancias en endpoints

--- Backend/routes/ComentarioRoutes.js
+++ Backend/routes/ComentarioRoutes.js
@@ -7,6 +7,6 @@ router.post("/comentario", comentario_controller.comentario_create);
 router.put("/comentario/:id", comentario_controller.comentario_update);
 router.delete("/comentario/:id", comentario_controller.comentario_delete);
 router.get("/comentario/:id", comentario_controller.comentario_getById);
-router.get("/comentario/:id", comentario_controller.comentario_getAll);
+router.get("/comentario/post/:id", comentario_controller.comentario_getAll);
 
 module.exports = router;
