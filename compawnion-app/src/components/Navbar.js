f88798822e9d809ddd73e65ee2385e9b6daf64ea
Rodrigo Yap <rodyap182@gmail.com>
2022-04-06 18:29:58 -0500

    Comienzo de la estilizacion del navbar

--- compawnion-app/src/components/Navbar.js
+++ compawnion-app/src/components/Navbar.js
@@ -1,15 +1,29 @@
 import React, { Fragment } from 'react'
 import { Link } from "react-router-dom";
+import { Box, Button, AppBar, Toolbar, Typography } from '@mui/material'
 
 export default function Navbar() {
   return (
-    <Fragment>
-        <nav className='navbar'>
-            <Link to="/">Home</Link>
-            <Link to="/perfil/:id">Perfil</Link>
-            <Link to="/busqueda">Buscar</Link>
-            <Link to="/">Sign Out</Link>
-        </nav>
-    </Fragment>
+    <Box sx={{flexGrow: 1}}>
+      <AppBar position='static'>
+        <Toolbar>
+          <Typography variant='h5' component="div" sx={{flexGrow: 1}}>
+            Compawnion
+          </Typography>
+          <Button component={Link} to="/" color='inherit' size="large">
+            Home
+          </Button>
+          <Button component={Link} to="/perfil/:id" color='inherit' size="large">
+            Perfil
+          </Button>
+          <Button component={Link} to="/busqueda" color='inherit' size="large">
+            Buscar
+          </Button>
+          <Button component={Link} to="/" color='inherit' size="large">
+            Sign Out
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   )
 }
